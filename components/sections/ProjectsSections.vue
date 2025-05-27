ac53b0c67411995221d1c44f52d4da3febe7953b
Yoki Jati Perkasa <yokijati@gmail.com>
2025-05-27 19:00:19 +0700

    add

--- components/sections/ProjectsSections.vue
+++ components/sections/ProjectsSections.vue
@@ -1,24 +1,45 @@
 <template>
   <!-- Container -->
-  <div class="min-h-screen py-4 md:py-10 b-primary flex max-w-full bp">
+  <div class="min-h-screen py-4 md:py-10 b-primary flex max-w-full">
     <!-- Container Content -->
-    <div class="w-full mx-auto flex flex-col md:flex-col gap-10 bb">
+    <div class="w-full mx-auto flex flex-col md:flex-col gap-10">
       <!-- Title Start -->
       <TitleSection> Projects </TitleSection>
       <!-- Title End -->
 
-      <div class="grid grid-cols-3 grid-rows-3 gap-4 by">
+      <div class="grid grid-cols-3 grid-rows-3 gap-12">
         <!-- Feliya Attendance -->
-        <div class="col-span-3 br">
+        <div class="col-span-3 flex flex-col gap-6">
           <!-- Image -->
-          <div></div>
+          <div>
+            <img
+              src="~/assets/images/feliya_attendance_app.png"
+              alt=""
+              class="rounded-2xl"
+            />
+          </div>
 
           <!-- Title & Description -->
-          <div>
+          <div class="flex flex-col gap-3">
             <!-- Title & Button View projects -->
             <div class="flex justify-between">
-              <h2>Feliya Attendance App</h2>
+              <h2 class="text-2xl font-medium">Feliya Attendance App</h2>
               <button>View Projects</button>
+            </div>
+
+            <!-- Description & Tech Stack -->
+            <div>
+              <!-- Tech Stack -->
+              <div></div>
+
+              <p>
+                Mengembangkan sistem manajemen kehadiran karyawan yang
+                komprehensif yang dirancang khusus untuk PT. Feliya Laju Teknik,
+                sebuah perusahaan jasa AC. Aplikasi seluler ini menyederhanakan
+                manajemen tenaga kerja dengan menyediakan pelacakan kehadiran
+                secara real-time, check-in/out berbasis GPS, dan kemampuan
+                pelaporan otomatis.
+              </p>
             </div>
           </div>
         </div>
