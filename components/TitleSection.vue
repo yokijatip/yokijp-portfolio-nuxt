028f6546c15a9c32f9f569aa68d45208caed2c1d
Yoki Jati Perkasa <yokijati@gmail.com>
2025-05-27 17:11:21 +0700

    Add layout

---
+++ components/TitleSection.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1 class="text-5xl font-medium"><slot></slot></h1>
+  </div>
+</template>
+
+<script setup></script>
+
+<style></style>
--- components/sections/ProjectsSections.vue
+++ components/sections/ProjectsSections.vue
@@ -2,9 +2,20 @@
   <!-- Container -->
   <div class="min-h-screen py-4 md:py-10 b-primary flex max-w-full bp">
     <!-- Container Content -->
-    <div
-      class="w-full mx-auto flex flex-col md:flex-row gap-10 items-center bb"
-    ></div>
+    <div class="w-full mx-auto flex flex-col md:flex-col gap-10 bb">
+      <!-- Title Start -->
+      <TitleSection> Projects </TitleSection>
+      <!-- Title End -->
+
+      <div class="grid grid-cols-3 grid-rows-3 gap-4 by">
+        <!-- Feliya Attendance -->
+        <div class="col-span-3 br">Feliya Attendance</div>
+        <div class="row-start-2 br">Feliya Company Profile</div>
+        <div class="row-start-2 br">Shelby Florist Bandung</div>
+        <div class="row-span-2 row-start-2 br">Klik Sewa</div>
+        <div class="col-span-2 br">Wedding Invitation Luxury</div>
+      </div>
+    </div>
   </div>
 </template>
 
